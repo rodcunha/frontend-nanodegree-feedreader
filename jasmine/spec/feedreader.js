4c7a70abcbf9d94db9c2c8789b392f4f98f3ac7d
Rodrigo C <rbpdvc@gmail.com>
2018-04-19 14:54:20 +0100

    implemented the first tests for the jasmine suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,30 +31,55 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('should have a url defined and not be empty', function() {
+           allFeeds.forEach(function(item) {
+             expect(item.url).toBeDefined();
+             expect(item.url).not.toBe('');
+           })
+         });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('should have a name defined and not be empty', function() {
+          allFeeds.forEach(function(item) {
+            expect(item.name).toBeDefined();
+            expect(item.name).not.toBe('');
+          });
+        });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('The menu', function() {
+        var body = document.body;
+        var menu = document.querySelectorAll('.menu-icon-link');
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+         it('Menu item should be hidden by default', function() {
+           expect(body.classList).toMatch('menu-hidden');
+         });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-
+          it('Menu should be visible when clicked', function() {
+            menu.addEventListener('click', function() {
+              expect(body.classList).toBe('');
+            });
+          });
+  });
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
+
+
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -69,4 +94,5 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    });
 }());
